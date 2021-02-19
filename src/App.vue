20262ef7cfb8fb18ab6bcdc31145bf2911a58c44
shachidaikon <tkroxy0815@gmail.com>
2021-02-20 00:29:57 +0900

    雛形

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Message
+    :message="message"
+    :displayName="displayName"
+    :icon="icon"
+    :timestamp="timestamp"
+    :isMine="isMine"
+  />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Message from './components/Message.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
-  }
-};
+    Message,
+  },
+  data: function() {
+    return {
+      id: '1',
+      message:
+        'これはサンプル。私は元気です。これはサンプル。私は元気です。これはサンプル。私は元気です。これはサンプル。私は元気です。これはサンプル。私は元気です。これはサンプル。私は元気です。これはサンプル。私は元気です。これはサンプル。私は元気です。',
+      icon: require('./assets/logo.png'),
+      timestamp: new Date(Date.now()),
+      displayName: 'Ryo Takayama',
+      isMine: true,
+    }
+  },
+}
 </script>
 
 <style>
---
+++ src/components/Message.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="wrapper" :class="{ wrapper__isMine: isMine }">
+    <img :src="icon" class="icon" />
+    <div class="card-wrapper">
+      <span class="display-name" :class="{ 'display-name__isMine': isMine }">{{
+        displayName
+      }}</span>
+      <div class="card" :class="{ card__isMine: isMine }">
+        <span class="content">{{ message }}</span>
+      </div>
+      <span class="date" :class="{ date__isMine: isMine }">{{ date }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  props: {
+    message: String,
+    timestamp: Object,
+    displayName: String,
+    icon: String,
+    isMine: Boolean,
+  },
+  computed: {
+    date: function() {
+      return this.timestamp.toLocaleString()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: grid;
+  max-width: 80%;
+  padding: 12px 0;
+  grid-template-columns: 48px 1fr;
+  grid-template-areas: 'areaA areaB';
+  &__isMine {
+    grid-template-columns: 1fr 48px;
+    grid-template-areas: 'areaB areaA';
+    margin: 0 0 0 auto;
+  }
+}
+.icon {
+  margin-top: 16px;
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  grid-area: areaA;
+}
+.card-wrapper {
+  grid-area: areaB;
+  margin: 0 24px;
+}
+.display-name {
+  margin: 0 16px;
+  font-size: 14px;
+  display: block;
+  &__isMine {
+    text-align: right;
+  }
+}
+.card {
+  border-radius: 18px;
+  padding: 14px;
+  line-height: 18px;
+  background: $color-secondary;
+  &__isMine {
+    background: $color-primary;
+  }
+}
+.date {
+  margin: 0 16px;
+  font-size: 14px;
+  display: block;
+  &__isMine {
+    text-align: right;
+  }
+}
+</style>
